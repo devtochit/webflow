7e68735279b328735d31f2ad20e74e57e3af3b6b
jesse uzoma <71229278+officailesse@users.noreply.github.com>
2022-06-16 09:58:28 -0600

    updated code

--- src/index.js
+++ src/index.js
@@ -1,78 +1,29 @@
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 import './style.css';
-import Sortable from 'sortablejs';
-import List from './modules/List.js';
-
-const ulList = document.getElementById('dynamic-list');
-let toDoLists = [];
-
-const viewPage = () => {
-  const local = new List();
-  toDoLists = local.getList();
-  toDoLists.sort((a, b) => a.index - b.index);
-  if (toDoLists.length) {
-    ulList.innerHTML = '';
-    toDoLists.forEach((list) => {
-      if (list) {
-
-        ulList.innerHTML += ` w
-                        <li id="${list.index - 1}" class = "todo-list" >
-                          <div class="list">
-        
-                            <label for="${list.index}">${list.description
-          }</label>
-                          </div>
-                          <button type="button" id= "${list.index - 1
-          }" class="deleteList">
-                          <i class="fa-solid fa-trash-can"></i>
-                          </button>
-                        </li>  
-                          `;
-      }
-    });
-  } else {
-    ulList.innerHTML = '<li>No score to Preview. Add New !</li>';
-  }
-
-  // eventlistener for deleteBtn
-  const buttons = document.querySelectorAll('.deleteList');
-  buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-      const localList = new List();
-      const buttonId = button.getAttribute('id');
-      localList.deleteList(buttonId * 1);
-      viewPage();
-    });
-  });
-
-  //   //drag and drop event listeners
-  const form = document.querySelector('#list-form');
-
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const localList = new List();
-    const value = document.querySelector('#add-list');
-    const score = document.querySelector('#add-score');
-    // const todoListLocal = localList.getList();
-
-    AddScoreToGame = ({
-      description: ` ${value.value} : ${score.value}`,
-    })
-    // localList.AddScoreToGame(todoListLocal);
-    value.value = '';
-    score.value = '';
-    viewPage();
-  });
-  const deleteChecked = document.querySelector('.delete-checked');
-  deleteChecked.addEventListener('click', () => {
-    const localList = new List();
-    localList.deleteCompleted();
-    viewPage();
-  });
+import { sendData, getData, newgame } from './modules/api'
 
 
-  const refresh = document.querySelector('.refresh-list');
-  refresh.addEventListener('click');
-}
+newgame()
+
+
+const refresh = document.querySelector('.refresh-list');
+refresh.addEventListener('click', getData);
+
+
+
+const submitBtn = document.getElementById('submit');
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const value = document.querySelector('#add-list').value;
+  const score = document.querySelector('#add-score').value;
+  return sendData({ value, score })
+});
+
+//   //drag and drop event listeners
+const form = document.querySelector('#list-form');
+
+
+
+
 
--- src/modules/api.js
+++ src/modules/api.js
@@ -1,23 +1,115 @@
 import axios from 'axios'
 export const GAME_ID = 'B9I6OaxhqWCQtc0Fe7m';
+const api = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 
-export const api = axios.create({
-    baseURL: 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/',
-})
 
-export const createGame = async (gameName) => {
-    const response = await api.post('/games', {
-        name: gameName
+
+
+// export const api = axios.create({
+//     baseURL: 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/',
+// })
+
+// export const createGame = async (gameName) => {
+//     const response = await api.post('/games', {
+//         name: gameName
+//     });
+//     return console.log(response.data.result.slice(15, 34))
+// }
+
+export const newgame = () => {
+    fetch(`${api}/games/`, {
+        method: 'POST',
+        body: JSON.stringify({
+            name: 'Jesse ',
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        },
+    })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+};
+
+
+
+export const sendData = async (data = {}) => {
+    await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/B9I6OaxhqWCQtc0Fe7m/scores/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
     });
-    return response.data.result.slice(15, 34)
-}
+    const value = document.querySelector('#add-list');
+    const score = document.querySelector('#add-score');
+    value.value = '';
+    score.value = '';
+};
 
-export const sendData = async (score) => {
-    await api.post(`/games/${GAME_ID}/scores`, score);
-}
+
+
+
+const ulList = document.getElementById('dynamic-list');
+
+const sortScores = (result) => {
+    const scores = [];
+    result.map((item) => scores.push(item.score));
+    return scores.sort((a, b) => b - a);
+};
+
+const getScoresWithNames = (result, sortedScores) => {
+    const finalResult = [];
+    sortedScores.map((score) => result.map((item) => (item.score === score
+        ? finalResult.push({ user: item.user, score })
+        : false)));
+    return finalResult;
+};
 
 export const getGameScores = async () => {
-    const response = await api.post(`/games/${GAME_ID}/scores`)
+    const response = await api.get(`/games/${GAME_ID}/scores`);
     return response.data.result;
+};
+
+export const getData = async () => {
+    const local = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/B9I6OaxhqWCQtc0Fe7m/scores');
+    const toDoLists = local.json()
+    return toDoLists.then((data) => {
+        ulList.innerHTML = '';
+        //sort scores 
+        const sortedScores = sortScores(data.result);
+        const finallData = getScoresWithNames(data.result, sortedScores);
+        // Display result 
+        return finallData.length ? finallData.map((item, index) => {
+            console.log(item.value)
+            if (index == 0) {
+                ulList.innerHTML += ` 
+                          <li id="${index}" class = "todo-list" >
+                            <div class="list">
+          
+                              <label for="${index}">${item.value}:${item.score}</label>
+                            </div>
+                            <button type="button" id= "${index}" class="deleteList">
+                            <i class="fa-solid fa-trash-can"></i>
+                            </button>
+                          </li>  
+                            `;
+            } else {
+                ulList.innerHTML += ` 
+        <li id="${index}" class = "todo-list" >
+          <div class="list">
+
+            <label for="${index}">${item.value}:${item.score}</label>
+          </div>
+          <button type="button" id= "${index}" class="deleteList">
+          <i class="fa-solid fa-trash-can"></i>
+          </button>
+        </li>  
+          `;
+            }
+
+        }) : finallData
+
+    })
 }
+
 
