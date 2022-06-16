fe867e441760ed8366e053f12214b67b24a5f080
jesse uzoma <71229278+officailesse@users.noreply.github.com>
2022-06-16 15:41:21 -0600

    added  fetch functionality

--- src/index.js
+++ src/index.js
@@ -1,29 +1,15 @@
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 import './style.css';
-import { sendData, getData, newgame } from './modules/api'
-
-
-newgame()
-
+import { getData } from './modules/api.js';
+import { getInputData } from './modules/util.js';
 
 const refresh = document.querySelector('.refresh-list');
 refresh.addEventListener('click', getData);
 
-
-
-const submitBtn = document.getElementById('submit');
+const submitBtn = document.querySelector('.submit');
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  const value = document.querySelector('#add-list').value;
-  const score = document.querySelector('#add-score').value;
-  return sendData({ value, score })
+
+  return getInputData();
 });
-
-//   //drag and drop event listeners
-const form = document.querySelector('#list-form');
-
-
-
-
-
--- src/modules/api.js
+++ src/modules/api.js
@@ -1,115 +1,86 @@
-import axios from 'axios'
-export const GAME_ID = 'B9I6OaxhqWCQtc0Fe7m';
+export const GAME_ID = 'C1VRLg8Jb50advt7iDv5';
 const api = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 
-
-
-
-// export const api = axios.create({
-//     baseURL: 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/',
-// })
-
-// export const createGame = async (gameName) => {
-//     const response = await api.post('/games', {
-//         name: gameName
-//     });
-//     return console.log(response.data.result.slice(15, 34))
-// }
-
 export const newgame = () => {
-    fetch(`${api}/games/`, {
-        method: 'POST',
-        body: JSON.stringify({
-            name: 'Jesse ',
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8'
-        },
-    })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+  fetch(`${api}games/`, {
+    method: 'POST',
+    body: JSON.stringify({
+      name: 'Jesse ',
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
 };
 
-
-
-export const sendData = async (data = {}) => {
-    await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/B9I6OaxhqWCQtc0Fe7m/scores/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    });
-    const value = document.querySelector('#add-list');
-    const score = document.querySelector('#add-score');
-    value.value = '';
-    score.value = '';
+export const sendData = async (userScore) => {
+  const newScore = await fetch(`${api}games/${GAME_ID}/scores`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ user: userScore.name, score: userScore.score }),
+  });
+  return newScore;
 };
-
-
-
 
 const ulList = document.getElementById('dynamic-list');
 
 const sortScores = (result) => {
-    const scores = [];
-    result.map((item) => scores.push(item.score));
-    return scores.sort((a, b) => b - a);
+  const scores = [];
+  result.map((item) => scores.push(item.score));
+  return scores.sort((a, b) => b - a);
 };
 
 const getScoresWithNames = (result, sortedScores) => {
-    const finalResult = [];
-    sortedScores.map((score) => result.map((item) => (item.score === score
-        ? finalResult.push({ user: item.user, score })
-        : false)));
-    return finalResult;
+  const finalResult = [];
+  sortedScores.map((score) => result.map((item) => (item.score === score
+    ? finalResult.push({ user: item.user, score })
+    : false)));
+  return finalResult;
 };
 
 export const getGameScores = async () => {
-    const response = await api.get(`/games/${GAME_ID}/scores`);
-    return response.data.result;
+  const response = await api.get(`/games/${GAME_ID}/scores`);
+  return response.data.result;
 };
 
 export const getData = async () => {
-    const local = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/B9I6OaxhqWCQtc0Fe7m/scores');
-    const toDoLists = local.json()
-    return toDoLists.then((data) => {
-        ulList.innerHTML = '';
-        //sort scores 
-        const sortedScores = sortScores(data.result);
-        const finallData = getScoresWithNames(data.result, sortedScores);
-        // Display result 
-        return finallData.length ? finallData.map((item, index) => {
-            console.log(item.value)
-            if (index == 0) {
-                ulList.innerHTML += ` 
+  const local = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/C1VRLg8Jb50advt7iDv5/scores/');
+  const toDoLists = local.json();
+
+  return toDoLists.then((data) => {
+    ulList.innerHTML = '';
+    // sort scores
+    const sortedScores = sortScores(data.result);
+    const finallData = getScoresWithNames(data.result, sortedScores);
+    // Display result
+
+        return finallData.length ? finallData.map((item, index) => {      // eslint-disable-line
+
+      if (index === 0) {
+        ulList.innerHTML += ` 
                           <li id="${index}" class = "todo-list" >
                             <div class="list">
           
                               <label for="${index}">${item.value}:${item.score}</label>
                             </div>
-                            <button type="button" id= "${index}" class="deleteList">
-                            <i class="fa-solid fa-trash-can"></i>
-                            </button>
+                           
                           </li>  
                             `;
-            } else {
-                ulList.innerHTML += ` 
+      } else {
+        ulList.innerHTML += ` 
         <li id="${index}" class = "todo-list" >
           <div class="list">
 
-            <label for="${index}">${item.value}:${item.score}</label>
+            <label for="${index}">${item.user}:${item.score}</label>
           </div>
-          <button type="button" id= "${index}" class="deleteList">
-          <i class="fa-solid fa-trash-can"></i>
-          </button>
+       
         </li>  
           `;
-            }
-
-        }) : finallData
-
-    })
-}
-
-
+      }
+    }) : finallData;
+  });
+};
